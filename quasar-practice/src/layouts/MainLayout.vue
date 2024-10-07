0cdb7601ee221eda4da60ae1e87a43c834117d0e
mustafa <mustafa@nodesol.com>
2024-10-07 22:51:24 +0000

    Header

--- quasar-practice/src/layouts/MainLayout.vue
+++ quasar-practice/src/layouts/MainLayout.vue
@@ -1,7 +1,35 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-  
 
+    <q-header class="bg-grey-5 text-black">
+      <div class="row">
+        <div class="col-10 q-mx-auto">
+          <div class="">
+
+            <q-list class="flex flex-center">
+              <q-item>
+                <q-item-section>
+                  <RouterLink class="text-black no-decoration" > Home</RouterLink>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+
+                  <RouterLink class="text-black no-decoration" > Product</RouterLink>
+                </q-item-section>
+              </q-item>
+              <q-item>
+                <q-item-section>
+                 <RouterLink class="text-black no-decoration" > Cart</RouterLink>
+                </q-item-section>
+              </q-item>
+
+
+            </q-list>
+          </div>
+        </div>
+      </div>
+    </q-header>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -17,3 +45,11 @@ defineOptions({
 
 
 </script>
+
+
+<style>
+
+    .no-decoration{
+      text-decoration: none
+    }
+</style>
